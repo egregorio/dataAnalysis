b008a0efcc27c4b07dfdb2e9afe4b7f3a19e488f
Elizabeth <egregorio@gwu.edu>
2022-04-21 11:58:47 -0700

    function to iterate through data files
    These functions iterate over all the csv files in a directory and average and dimensionalize them.
    
    This is the first working version with comments to understand how it works

--- dimensional_functions.py
+++ dimensional_functions.py
@@ -16,28 +16,39 @@ def iterate_over_files(csv_path,directory_path,experiment_constants,save_path,sa
         hoi = constants[2] # Height Of Impact
         xoi = constants[3] # X coordinate of Impact
 
+        # Iterate over all the *.csv files in the directory
         for filename in glob.glob(csv_path):
+                # Declare the current filename, a *.csv in the path
                 current_file = os.path.join(directory_path,filename)
+                # Load the current file into a numpy array
                 trackingArray = np.loadtxt(current_file,delimiter=',',skiprows=0)
+                # Call the function to dimensionalize the data, changes pixels to meters
                 x_array, y_array, x_length, y_length = dimensionalize(trackingArray,fps,ppm,hoi,xoi)
 
+                # Creates the list of all the position lists
                 all_x_array.append(x_array)
                 all_y_array.append(y_array)
 
+                # Creates the list of all the length lists
                 all_x_length.append(x_length)
                 all_y_length.append(y_length)
 
+        # Finds the maximum length of a list
         x_max_length = max(all_x_length)
         y_max_length = max(all_y_length)
 
+        # Padds the shorter lists with zeros so they are all the same length
         padded_x = create_one(all_x_array, x_max_length)
         padded_y = create_one(all_y_array, y_max_length)
 
+        # Finds the average position at every time step
         means_x, x_stds = averageF( padded_x, x_max_length)
         means_y, y_stds = averageF( padded_y, y_max_length)
 
+        # Converts the frames to seconds
         timeStamp = frames_to_sec(means_x)
 
+        # Creates and saves an array that has all the values in their own row
         save_array = zip(timeStamp, means_x, x_stds, means_y, y_stds)
         please_save = os.path.join(save_path,save_name)
         np.savetxt(please_save,save_array)        
@@ -49,37 +60,42 @@ def dimensionalize(data_file,frames_per_second,pixels_per_m,height_of_impact,x_o
         x_list = []
         y_list = []
 
-        cutoff = 250
+        cutoff = 250 # The shortest height we care about
 
-        length = len(data_file)
+        length = len(data_file) # Finds the length of the current data file
 
-        for i in range(0,length):
-                current_x = float(data_file[i,1])
-                current_y = float(data_file[i,0])
+        for i in range(0,length): # Iterates over every row of the data file
+                current_x = float(data_file[i,1]) # This rows x coordinate
+                current_y = float(data_file[i,0]) # This rows y coordinate
 
-                if current_y > cutoff:
-                        if current_y < height_of_impact:
-                                x_list = np.append(x_list, (current_x - x_of_impact) / pixels_per_m)
-                                y_list = np.append(y_list, (current_y - height_of_impact) / pixels_per_m )
+                if current_y > cutoff: # Uses the y coordinates to find the range we care about
+                        if current_y < height_of_impact: # Only heights between impact and hitting bumpers
+                                x_list = np.append(x_list, ( current_x - x_of_impact) / pixels_per_m )
+                                y_list = np.append(y_list, ( current_y - height_of_impact) / pixels_per_m )
                         
                 i = i + 1
-        
+
+        # Finds the length of the lists created 
         length_x = len(x_list)
         length_y = len(y_list)
 
+        # Returns the x and y coordinate litsts and their lengths
         return x_list, y_list, length_x, length_y
 
+# This function padds the arrays with zeros so they're all the same length
 def padding(sample, max_length):
     if len(sample)<max_length:
         zero_ar = [0]*(max_length-len(sample))
         sample = np.append(sample,zero_ar)
     return sample
 
+# This function combines all the padded arrays into one array
 def create_one(input_ar, max_length):
     for i in range(0, len(input_ar)):
         input_ar[i] = padding(input_ar[i], max_length)
     return input_ar
 
+# This function finds the average x,y coordinate at every time
 def averageF(all_samples, max_length):
     mean_list =[]
     stdv_list =[]
@@ -94,6 +110,7 @@ def averageF(all_samples, max_length):
         stdv_list.append(stdv_at_specific_x)
     return mean_list,stdv_list
 
+# This function converts the frame list to a time list
 def frames_to_sec(position_list):
     time_list = []
     length = len(position_list)
