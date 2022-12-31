2398b9b6d7aa678f889f1a109eef7ccb5719c821
Elizabeth <egregorio@gwu.edu>
2022-12-31 11:12:55 -0500

    changes to code, base directory updated
    created a new archive folder to hold all the plot and function files that aren't currently being used

--- test.py
+++ test.py
@@ -3,12 +3,17 @@ import glob
 import os
 from dimensional_functions import *
 
-path_for_joining   = '/Users/elizabeth/Box Sync/dataAnalysis/data_to_plot/04052022_asym3'
-path_with_only_csv = '/Users/elizabeth/Box Sync/dataAnalysis/data_to_plot/04052022_asym3/*.csv'
-path_to_constants  = '/Users/elizabeth/Box Sync/dataAnalysis/experiment_constants/04052022_asym1_constants.txt'
-path_to_save       = '/Users/elizabeth/Box Sync/dataAnalysis/dimensionalData'
-name_to_save       = '04052022_asym3_data'
-name_to_file       = '04052022_asym3_filesList'
+path_for_joining   = '/Users/elizabeth/egregorio@gwmail.gwu.edu - Google Drive/My Drive/Box Sync/dataAnalysis/data_to_plot/\
+04052022_asym3'
+path_with_only_csv = '/Users/elizabeth/egregorio@gwmail.gwu.edu - Google Drive/My Drive/Box Sync/dataAnalysis/data_to_plot/\
+04052022_asym3/*.csv'
+path_to_constants  = '/Users/elizabeth/egregorio@gwmail.gwu.edu - Google Drive/My Drive/Box Sync/dataAnalysis/experiment_constants/\
+04052022_asym1_constants.txt'
+path_to_save       = '/Users/elizabeth/egregorio@gwmail.gwu.edu - Google Drive/My Drive/Box Sync/dataAnalysis/dimensionalData'
+name_to_save       = \
+'04052022_asym3_data'
+name_to_file       = \
+'04052022_asym3_filesList'
 
 iterate_over_files(path_with_only_csv,path_for_joining,path_to_constants,path_to_save,name_to_save,name_to_file)
 
